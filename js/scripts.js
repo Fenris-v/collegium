3ae3248af98bdeda76bd501fad338f1facf67964
Fenris <anatolijv236@gmail.com>
2020-05-10 18:46:01 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+    /**
+     * HAMBURGER
+     */
+    $('.menu-toggle-inner').on('click', function (e) {
+        $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+        $('.headerNav_adaptive').toggleClass('header_opened');
+    });
+
+    /**
+     * FIRST SCREEN SLIDER
+     */
+    $('.firstScreen__slider').slick({
+        slidesToShow: 1,
+        arrows: false,
+        infinite: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        speed: 2000,
+        draggable: false,
+        pauseOnFocus: false
+    });
+
+    $('.projects__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        margin: 10,
+        arrows: true,
+        infinite: false,
+        dots: false,
+        autoplay: false,
+        draggable: false
+    });
+});
